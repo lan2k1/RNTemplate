cd830826e1f52d91a20a3474b2cc5ce460532f29
Lan Pham <lanpcd@dgroup.com>
2023-02-23 21:04:20 +0700

    feat: install some package

---
+++ src/constants/fonts.ts
@@ -0,0 +1,19 @@
+export const FONTS = {
+  FAMILY: {
+    REGULAR: 'SourceSansPro-Regular',
+    SEMI_BOLD: 'SourceSansPro-SemiBold',
+  },
+
+  SIZE: {
+    h2: 31,
+    h3: 25,
+    h4: 20,
+    h5: 16,
+    subhead: 12,
+    small: 10,
+    caption: 12,
+    body1: 16,
+    body2: 20,
+    body3: 25,
+  },
+};
